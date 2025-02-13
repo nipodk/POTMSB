6672e76c206c44181cb2bdead4f7519e5ac37424
nipodk <nikitapodkopaevco@gmail.com>
2025-02-13 11:11:01 +0200

    POTMS-1 * init

---
+++ src/main/java/com/poweroftwo/potms_backend/PotmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.poweroftwo.potms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poweroftwo/potms_backend/PotmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poweroftwo.potms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
